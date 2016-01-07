02d1fc099cc033e1f590361ad265b0c261d80286
nhtoshiaki <nh_toshi.94@hotmail.com>
2016-01-07 11:48:36 -0200

    changing db model

--- bd/sql_scripts/create.sql
+++ bd/sql_scripts/create.sql
@@ -7,18 +7,21 @@ create table if not exists Pesquisador (
     nome varchar(45) not null
 );
 
-create table if not exists Tag (
+create table if not exists Artigo (
     id integer auto_increment primary key,
-    nomeTag varchar(45) not null
+    nome varchar(45) not null
 );
 
-create table if not exists Tag_Pesquisador (
-    idPesquisador integer not null,
-    idTag integer not null,
-    peso integer not null default 1,
-    primary key (idPesquisador, idTag),
-    constraint fk_pesquisador
-        foreign key (idPesquisador) references Pesquisador(id),
-    constraint fk_tag
-        foreign key (idTag) references Tag(id)
+create table if not exists Autoria (
+    idPesquisador integer primary key,
+    idArtigo integer primary key,
+    constraint fk_pesq_autoria
+        foreign key (idPesquisador) references Pesquisador (id),
+    constraint fk_artigo_autoria
+        foreign key (idArtigo) references Artigo (id)
+);
+
+create table if not exists Tag (
+    id integer auto_increment primary key,
+    nomeTag varchar (50) not null
 );
