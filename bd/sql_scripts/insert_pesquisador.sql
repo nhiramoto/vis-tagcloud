d154e6be8ce0864746968a2a8481d77573f8d925
Nicolau <nh_toshi.94@hotmail.com>
2016-05-30 14:43:01 -0300

    more articles

--- bd/sql_scripts/insert_pesquisador.sql
+++ bd/sql_scripts/insert_pesquisador.sql
@@ -1,10 +1,10 @@
 use Tagcloud;
 
-insert into Pesquisador(id, nome, depart) value (1, "Artero A.O.", "Matemática e Computação");
-insert into Pesquisador(id, nome, depart) value (2, "Olivete Junior C.", "Matemática e Computação");
-insert into Pesquisador(id, nome, depart) value (3, "Eler D.M.", "Matemática e Computação");
-insert into Pesquisador(id, nome, depart) value (4, "Piteri M.A.", "Matemática e Computação");
-insert into Pesquisador(id, nome, depart) value (5, "Dias M.A.", "Matemática e Computação");
-insert into Pesquisador(id, nome, depart) value (6, "Shimabukuru M.H.", "Matemática e Computação");
-insert into Pesquisador(id, nome, depart) value (7, "Garcia R.E.", "Matemática e Computação");
-insert into Pesquisador(id, nome, depart) value (8, "Correia R.M.", "Matemática e Computação");
+insert into Pesquisador(id, nome, depart) value (1, "Artero Almir.O.", "Matemática e Computação");
+insert into Pesquisador(id, nome, depart) value (2, "Olivete Junior Celso.", "Matemática e Computação");
+insert into Pesquisador(id, nome, depart) value (3, "Eler Danilo.M.", "Matemática e Computação");
+insert into Pesquisador(id, nome, depart) value (4, "Piteri Marco.A.", "Matemática e Computação");
+insert into Pesquisador(id, nome, depart) value (5, "Dias Maurício.A.", "Matemática e Computação");
+insert into Pesquisador(id, nome, depart) value (6, "Shimabukuru Milton.H.", "Matemática e Computação");
+insert into Pesquisador(id, nome, depart) value (7, "Garcia Rogério.E.", "Matemática e Computação");
+insert into Pesquisador(id, nome, depart) value (8, "Correia Ronaldo.M.", "Matemática e Computação");
